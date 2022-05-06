1a250f5cb85999c3e29e4d016e82c473766f70c4
Juanes07 <juanesdm3@gmail.com>
2022-05-06 09:44:37 -0500

    Separacion Componentes, Reducer y StoreProvider
    Se hace la separacion de los difrentes componentes, adicional reducer y StoreProvider se separan en carpetas respectivas.

---
+++ front/src/components/List.jsx
@@ -0,0 +1,104 @@
+import React, {useContext,useEffect } from 'react';
+import { HOST_API } from '../App';
+import { Store } from '../App';
+
+const  List =  () => {
+  const {
+    dispatch,
+    state: { todo },
+  } = useContext(Store);
+  const currentList = todo.list;
+
+  useEffect(() => {
+    fetch(HOST_API + "/todos")
+      .then((response) => response.json())
+      .then((list) => {
+        dispatch({ type: "update-list", list });
+      });
+  }, [dispatch]);
+
+  const onDelete = (id) => {
+    fetch(HOST_API + "/" + id + "/todo", {
+      method: "DELETE",
+    }).then((list) => {
+      dispatch({ type: "delete-item", id });
+    });
+  };
+
+  const onEdit = (todo) => {
+    dispatch({ type: "edit-item", item: todo });
+  };
+
+  const onChange = (event, todo) => {
+    const request = {
+      name: todo.name,
+      id: todo.id,
+      completed: event.target.checked,
+    };
+
+    fetch(HOST_API + "/todo", {
+      method: "PUT",
+      body: JSON.stringify(request),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+      .then((response) => response.json())
+      .then((todo) => {
+        dispatch({ type: "update-item", item: todo });
+      });
+  };
+
+  const decorationDone = {
+    textDecoration: "line-through",
+  };
+
+  return (
+    <div>
+      <table>
+        <thead>
+          <tr>
+            <td>¿Completado?</td>
+            <td>Tarea</td>
+          </tr>
+        </thead>
+        <tbody>
+          {currentList.map((todo) => {
+            return (
+              <tr key={todo.id} style={todo.completed ? decorationDone : {}}>
+                <td>
+                  <input
+                    type="checkbox"
+                    defaultChecked={todo.completed}
+                    onChange={(event) => onChange(event, todo)}
+                  ></input>
+                </td>
+                <td>{todo.name}</td>
+                <td>
+                  <button className="List_btn" onClick={() => onEdit(todo)}>
+                    <img
+                      className="Icon"
+                      alt="edit"
+                      src="https://cdn-icons-png.flaticon.com/512/747/747825.png"
+                    ></img>
+                  </button>
+                </td>
+                <td>
+                  <button className="List_btn" onClick={() => onDelete(todo.id)}>
+                    <img
+                      className="Icon"
+                      alt="delete"
+                      src="https://cdn-icons-png.flaticon.com/512/5602/5602138.png"
+                    ></img>
+                  </button>
+                </td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+    </div>
+  );
+}
+
+export default List;
